8aaf9e93efe41190ad4021b984b597b5ca4adf09
Ha Nguyen Quang <quanghait@gmail.com>
2023-09-27 10:36:53 +0700

    Interface Segregation Principle

---
+++ Interface_Segregation_Principle/Class/Eagle.cs
@@ -0,0 +1,17 @@
+﻿using Interface_Segregation_Principle.Interface;
+
+namespace Interface_Segregation_Principle.Class
+{
+    public class Eagle: IFlyableAnimal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("The Eagle is eating...");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("The Eagle is flying...");
+        }
+    }
+}
---
+++ Interface_Segregation_Principle/Interface/IFlyableAnimal.cs
@@ -0,0 +1,7 @@
+﻿namespace Interface_Segregation_Principle.Interface
+{
+    public interface IFlyableAnimal: IAnimal
+    {
+        void Fly();
+    }
+}
